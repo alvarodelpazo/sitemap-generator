f30b9128f36b6b5dd2f30f757e8ce4a611a3bc04
Álvaro del Pazo Batista <83280260+alvarodelpazo@users.noreply.github.com>
2024-01-05 08:25:19 +0000

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const sitemapService: {
-    createSitemap: () => void;
-    processNode: (originalNode: import("./node/Node").Node<Record<string, unknown>>) => import("./node/Node").Node<Record<string, unknown>>;
-    bootstrapProcessors: () => void;
-};
--- dist/node/Node.d.ts
+++
@@ -1,6 +0,0 @@
-import { ContentNode } from './ContentNode';
-export interface Node<T = Record<string, unknown>> {
-    data: {
-        content: ContentNode<T>;
-    };
-}
